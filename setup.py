68ee38392e9075a1f50cfeaf774d4a1f9da0fc1d
warpdynamics <wojcik@warpdynamics.co.uk>
2017-03-17 13:10:02 +0000

    added new files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+setup(
+    name="sss",
+    version="0.1.0",
+    install_requires=requirements,
+    author="Michal Wojcik",
+    author_email="michal.s.wojcik@gmail.com",
+    description=(""),
+    license="BSD",
+    keywords="",
+    url="",
+    packages=find_packages(),
+)
