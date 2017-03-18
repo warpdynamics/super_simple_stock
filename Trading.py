783f651ac8ad5c8e007472971e3effd14508b6a9
warpdynamics <wojcik@warpdynamics.co.uk>
2017-03-18 14:45:02 +0000

    solution

--- Trading.py
+++ Trading.py
@@ -1,6 +1,7 @@
 from collections import deque
 from decimal import Decimal  # to provide sufficient accuracy for financial arithmetic
 import datetime
+import math
 
 
 class Trade(object):
@@ -8,6 +9,8 @@ class Trade(object):
     sell = 1
 
     def __init__(self, indicator, price, quantity):
+        price = Decimal(price)
+        quantity = Decimal(quantity)
         if price <= 0 or quantity <= 0 or (indicator != Trade.buy and indicator != Trade.sell):
             raise ValueError
         self.indicator = indicator
@@ -16,14 +19,29 @@ class Trade(object):
 
 
 class Stock(object):
+    """
+    class Stock is representing a single stock
+    """
 
-    def __init__(self, stock_symbol, trades_expiry_time):
+    def __init__(self, stock_symbol, trades_expiry_time, par_value, fixed_dividen=None):
+        """
+        :param stock_symbol: string stock symbol
+        :param trades_expiry_time: how long trades should be remember to calculate share price is seconds [int]
+        :param par_value: Par Value in given currency units e.g in GBP [string]
+        :param fixed_dividen: [string]
+        :return: None
+        """
         self.stock_symbol = stock_symbol
         self.last_trades = deque()
         self.sum = Decimal(0)
         self.volume = Decimal(0)
-        self.full_history = []
         self.trades_expiry_time = trades_expiry_time
+        self.par_value = Decimal(par_value)
+        if fixed_dividen is not None:
+            self.fixed_dividend = Decimal(fixed_dividen)
+        else:
+            self.fixed_dividend = None
+        self.last_dividend = Decimal(0)
 
     def __add_trade(self, trade, t):
         """
@@ -34,9 +52,8 @@ class Stock(object):
         """
 
         self.last_trades.append((t, trade))
-        self.full_history.append((t, trade))
-        self.sum += Decimal(trade.price) * Decimal(trade.quantity)
-        self.volume += Decimal(trade.quantity)
+        self.sum += trade.price * trade.quantity
+        self.volume += trade.quantity
 
     def __remove_oldest_trade(self):
         """
@@ -44,8 +61,8 @@ class Stock(object):
         :return: None
         """
         (_, trade) = self.last_trades.popleft()
-        self.sum -= Decimal(trade.price) * Decimal(trade.quantity)
-        self.volume -= Decimal(trade.quantity)
+        self.sum -= trade.price * trade.quantity
+        self.volume -= trade.quantity
 
     def __clean_last_trades(self, t):
         while len(self.last_trades) > 0:
@@ -78,3 +95,57 @@ class Stock(object):
         if len(self.last_trades) == 0:
             return Decimal(0)
         return self.sum/self.volume
+
+    def record_dividend(self, value):
+        """
+        :param value: value of dividend in given currency units e.g in GBP [string]
+        :return: None
+        """
+        self.last_dividend = Decimal(value)
+
+    def dividend_yield(self, t=None):
+        price = self.get_price(t)
+
+        if price == 0:
+            return Decimal(0)
+
+        if self.fixed_dividend is None:
+            return self.last_dividend/price
+        else:
+            return (self.fixed_dividend * self.par_value)/price
+
+    def get_pe_ratio(self, t=None):
+        price = self.get_price(t)
+        if self.last_dividend == 0:
+            return Decimal(0)
+
+        return price/self.last_dividend
+
+
+class Market(object):
+    def __init__(self, trades_expiry_time):
+        self.stocks = {}
+        self.trades_expiry_time = trades_expiry_time
+
+    def insert_stock(self, stock_symbol, par_value, fixed_dividen=None):
+        stock = Stock(stock_symbol, self.trades_expiry_time, par_value, fixed_dividen)
+        if stock.stock_symbol in self.stocks:
+            raise Exception("This Stock is already in the market")
+
+        self.stocks[stock.stock_symbol] = stock
+
+    def get_stock(self, stock_symbol):
+        if stock_symbol in self.stocks:
+            return self.stocks[stock_symbol]
+        else:
+            return None
+
+    def get_index(self, t=None):
+        if len(self.stocks) == 0:
+            return 0
+        if t is None:
+            t = datetime.datetime.utcnow()
+        try:
+            return math.exp(sum([math.log(self.get_stock(stock_symbol).get_price(t)) for stock_symbol in self.stocks])/float(len(self.stocks)))
+        except ValueError:
+            return 0
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from trading import Trade, Stock
+from trading import Trade, Stock, Market
 from datetime import datetime
 from decimal import Decimal
+import random
+import math
 
 import unittest
 
@@ -11,15 +13,88 @@ def ts(t):
 
 class Test(unittest.TestCase):
 
-    def test_stocks(self):
-        stock = Stock("USD", 900)
-        stock.record_trade(Trade(Trade.buy, Decimal('10'), Decimal('100')), ts(0))
-        stock.record_trade(Trade(Trade.buy, Decimal('11'), Decimal('400')), ts(200))
+    def test_stocks1(self):
+        stock = Stock("TEA", 900, '1')
+        stock.record_dividend('3.20')
+        stock.record_trade(Trade(Trade.buy, '10', '100'), ts(0))
+        stock.record_trade(Trade(Trade.buy, '11', '400'), ts(200))
         self.assertEqual(stock.get_price(ts(200)), Decimal('10.8'))
-        stock.record_trade(Trade(Trade.buy, Decimal('12'), Decimal('400')), datetime.fromtimestamp(1000))
+        stock.record_trade(Trade(Trade.buy, '12', '400'), ts(1000))
         self.assertEqual(stock.get_price(ts(1001)), Decimal('11.5'))
+        self.assertEqual(round(stock.dividend_yield(ts(1001)), 4), Decimal('0.2783'))
         self.assertEqual(stock.get_price(ts(1800)), Decimal('12'))
+        self.assertEqual(round(stock.dividend_yield(ts(1800)), 4), Decimal('0.2667'))
         self.assertEqual(stock.get_price(ts(2000)), Decimal('0'))
+        self.assertEqual(stock.dividend_yield(ts(2000)), Decimal('0'))
+
+        stock = Stock("GIN", 900, '1', '0.02')
+        stock.record_trade(Trade(Trade.buy, '13.45', '100'), ts(0))
+        stock.record_trade(Trade(Trade.buy, '16.77', '400'), ts(200))
+        self.assertEqual(stock.get_price(ts(200)), Decimal('16.106'))
+        stock.record_trade(Trade(Trade.buy, '12.01', '711'), ts(1000))
+        self.assertEqual(round(stock.get_price(ts(1001)), 4), Decimal('13.7238'))
+        self.assertEqual(round(stock.dividend_yield(ts(1001)), 4), Decimal('0.0015'))
+        self.assertEqual(stock.get_price(ts(1800)), Decimal('12.01'))
+        self.assertEqual(round(stock.dividend_yield(ts(1800)), 4), Decimal('0.0017'))
+        self.assertEqual(stock.get_price(ts(2000)), Decimal('0'))
+        self.assertEqual(stock.dividend_yield(ts(2000)), Decimal('0'))
+
+    def test_stocks2(self):
+        expire = 900
+        stock = Stock("TEA", expire, '1')
+        tracker = []
+        for T in range(0, 2000):
+            val = Decimal(10 + random.random())
+            qty = Decimal(random.randint(100, 200))
+            tracker.append((T, val, qty))
+            trade = Trade(Trade.buy, val, qty)
+            stock.record_trade(trade, ts(T))
+            price1 = stock.get_price(ts(T))
+
+            filtered = list(filter(lambda k: T - k[0] <= expire, tracker))
+            price2 = sum(map(lambda k: k[1]*k[2], filtered))/sum(map(lambda k: k[2], filtered))
+
+            self.assertAlmostEqual(price1, price2)
+
+    def test_market(self):
+        market = Market(900)
+        market.insert_stock("TEA", '1')
+        market.insert_stock("GIN", '1', '0.02')
+
+        stock_tea = market.get_stock("TEA")
+        stock_gin = market.get_stock("GIN")
+
+        stock_tea.record_dividend('3.20')
+        stock_tea.record_trade(Trade(Trade.buy, '10', '100'), ts(0))
+        stock_gin.record_trade(Trade(Trade.buy, '13.45', '100'), ts(100))
+        stock_tea.record_trade(Trade(Trade.buy, '11', '400'), ts(200))
+        stock_gin.record_trade(Trade(Trade.buy, '16.77', '400'), ts(500))
+
+        self.assertEqual(stock_tea.get_price(ts(600)), Decimal('10.8'))
+        self.assertEqual(stock_gin.get_price(ts(600)), Decimal('16.106'))
+
+        self.assertAlmostEqual(market.get_index(ts(600)), math.sqrt(Decimal('10.8')*Decimal('16.106')))
+
+    def test_market_no_ts(self):
+        market = Market(900)
+        market.insert_stock("TEA", '1')
+        market.insert_stock("GIN", '1', '0.02')
+
+        stock_tea = market.get_stock("TEA")
+        stock_gin = market.get_stock("GIN")
+
+        stock_tea.record_dividend('3.20')
+        stock_tea.record_trade(Trade(Trade.buy, '10', '100'))
+        stock_gin.record_trade(Trade(Trade.buy, '13.45', '100'))
+        stock_tea.record_trade(Trade(Trade.buy, '11', '400'))
+        stock_gin.record_trade(Trade(Trade.buy, '16.77', '400'))
+
+        self.assertEqual(stock_tea.get_price(), Decimal('10.8'))
+        self.assertAlmostEqual(stock_tea.dividend_yield(), Decimal('3.20')/Decimal('10.8'))
+        self.assertEqual(stock_gin.get_price(), Decimal('16.106'))
+        self.assertAlmostEqual(stock_gin.dividend_yield(), Decimal('0.02')/Decimal('16.106'))
+
+        self.assertAlmostEqual(market.get_index(), math.sqrt(Decimal('10.8')*Decimal('16.106')))
 
 if __name__ == '__main__':
     unittest.main()
